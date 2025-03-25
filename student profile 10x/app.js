d80fe3406ee4ba5af3cc936ddbce8cabf0817bb6
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-03-25 15:42:57 +0400

    Student Profile

---
+++ student profile 10x/app.js
@@ -0,0 +1,65 @@
+function generateJson() {
+    const fullName = document.querySelector("[name='full-name']").value;
+    const email = document.querySelector("[name='email']").value;
+    const country = document.querySelector("[name='all-country']").value;
+    const phone = document.querySelector("[name='phone']").value;
+    const finishedUniversity = document.querySelector("[name='finished-university-years']").value;
+
+  const jsonLd = {
+    "type": "object",
+    "properties": [
+        {
+        "type": "string",
+        "name": "fullName",
+        "label": "full-name",
+        "required": true,
+        "value": fullName
+        },
+        {
+        "type": "string",
+        "name": "email",
+        "label": "Email",
+        "inputType": "email",
+        "required": true,
+        "value": email
+        },
+        {
+        "type": "enum",
+        "name": "country",
+        "label": "all-country",
+        "options": [
+            {
+            "value": "geo",
+            "label": "Georgia"
+            },
+            {
+            "value": "usa",
+            "label": "Unaited States of America"
+            },
+            {
+            "value": "uk",
+            "label": "Unaited Kingdom"
+            }
+        ]
+        
+        },
+        {
+            "type": "string",
+            "name": "phone",
+            "label": "Phone",
+            "value": phone 
+        },
+        {
+            "type": "number",
+            "name": "finishedUniversity",
+            "label": "Finished University Years",
+            "value": finishedUniversity
+        }
+    ]
+}
+
+  document.getElementById('output').value = JSON.stringify(jsonLd, null , 2)
+}
+
+
+console.log(generateJson);
